3982f69cbe886c9e79d6f86da0a0a6082d9432a4
Milena Gongra <milena.gongra@indicium.tech>
2026-07-28 00:32:14 +0000

    ajuste no cast da coluna dos status

--- models/intermediate/sales_adwork/int_sales__joined_orders.sql
+++ models/intermediate/sales_adwork/int_sales__joined_orders.sql
@@ -4,7 +4,7 @@ with
         from {{ ref('stg_adwork__orders') }}
     )
 
-    , orderdetail as (
+    , order_details as (
         select *
         from {{ ref('stg_adwork__order_details') }}
     )
@@ -16,36 +16,38 @@ with
 
     , joined as (
         select
-            orderdetail.order_item_sk
+            order_details.order_item_sk
             , orders.order_pk
             , coalesce(orders.customer_fk, -1) as customer_fk
             , coalesce(orders.credit_card_fk, -1) as credit_card_fk
             , coalesce(orders.address_fk, -1) as address_fk
-            , orderdetail.product_fk
+            , order_details.product_fk
             , orders.order_date
-            , case orders.order_status
-                when 1 then 'In Process'
-                when 2 then 'Approved'
-                when 3 then 'Backordered'
-                when 4 then 'Rejected'
-                when 5 then 'Shipped'
-                when 6 then 'Cancelled'
-                else 'Unknown'
-            end as order_status
+            , cast(
+                case orders.order_status
+                    when 1 then 'In Process'
+                    when 2 then 'Approved'
+                    when 3 then 'Backordered'
+                    when 4 then 'Rejected'
+                    when 5 then 'Shipped'
+                    when 6 then 'Cancelled'
+                    else 'Unknown'
+                end as string
+            ) as order_status
             , coalesce(order_reasons.sales_reason_name, 'Not Informed') as sales_reason_name
-            , orderdetail.quantity
-            , orderdetail.unit_price
-            , orderdetail.discount_pct
-            , (orderdetail.quantity * orderdetail.unit_price) as gross_amount
+            , order_details.quantity
+            , order_details.unit_price
+            , order_details.discount_pct
+            , (order_details.quantity * order_details.unit_price) as gross_amount
             , (
-                (orderdetail.quantity * orderdetail.unit_price) * orderdetail.discount_pct
+                (order_details.quantity * order_details.unit_price) * order_details.discount_pct
             ) as discount_amount
             , (
-                (orderdetail.quantity * orderdetail.unit_price) * (1 - orderdetail.discount_pct)
+                (order_details.quantity * order_details.unit_price) * (1 - order_details.discount_pct)
             ) as net_amount
         from orders
-        inner join orderdetail
-            on orders.order_pk = orderdetail.order_fk
+        inner join order_details
+            on orders.order_pk = order_details.order_fk
         left join order_reasons
             on orders.order_pk = order_reasons.order_fk
     )
