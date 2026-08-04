373916c34f90a65e7883c3b1c0da3f4c4ac9b26f
Milena Gongra <milena.gongra@indicium.tech>
2026-08-04 14:22:32 +0000

    correção de acento

--- models/staging/sales_adwork/stg_adwork__country_regions.sql
+++ models/staging/sales_adwork/stg_adwork__country_regions.sql
@@ -4,12 +4,30 @@ with
         from {{ source('adwork', 'person_countryregion') }}
     )
 
-    , renamed as (
+    , cleaned as (
         select
             cast(countryregioncode as string) as country_region_pk
-            , cast(name as string) as country_name
+            , cast(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                    coalesce(name, 'Not Informed')
+                    , 'Ã¼', 'ü')
+                    , 'Ã¶', 'ö')
+                    , 'Ã¤', 'ä')
+                    , 'Ã©', 'é')
+                    , 'Ã¨', 'è')
+                    , 'Ã£', 'ã')
+                    , 'Ã§', 'ç')
+                    , 'Ã¡', 'á')
+            as string) as country_name
         from source_person_countryregion
     )
 
 select *
-from renamed
+from cleaned
--- models/staging/sales_adwork/stg_adwork__person_addresses.sql
+++ models/staging/sales_adwork/stg_adwork__person_addresses.sql
@@ -4,14 +4,51 @@ with
         from {{ source('adwork', 'person_address') }}
     )
 
-    , renamed as (
+    , cleaned as (
         select
             cast(addressid as int) as address_pk
-            , cast(addressline1 as string) as address_line
-            , cast(city as string) as city_name
             , cast(stateprovinceid as int) as state_province_fk
+            , cast(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                    addressline1
+                    , 'Ã¼', 'ü')
+                    , 'Ã¶', 'ö')
+                    , 'Ã¤', 'ä')
+                    , 'Ã©', 'é')
+                    , 'Ã¨', 'è')
+                    , 'Ã£', 'ã')
+                    , 'Ã§', 'ç')
+                    , 'Ã¡', 'á')
+            as string) as address_line
+            , cast(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                replace(
+                    city
+                    , 'Ã¼', 'ü')
+                    , 'Ã¶', 'ö')
+                    , 'Ã¤', 'ä')
+                    , 'Ã©', 'é')
+                    , 'Ã¨', 'è')
+                    , 'Ã£', 'ã')
+                    , 'Ã§', 'ç')
+                    , 'Ã¡', 'á')
+            as string) as city_name
+
         from source_person_address
     )
 
 select *
-from renamed
+from cleaned
